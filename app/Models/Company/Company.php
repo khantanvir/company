011e9ece4ba237c8b68aa7a017d2d9126c505a0c
tanvir <aiub.tanvir@gmail.com>
2022-08-13 02:05:18 +0600

    first commit to company

---
+++ app/Models/Company/Company.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Company;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+    protected $table = "companies";
+}
